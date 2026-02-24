30fda9163b1ebd5c04159429543b30ae94179015
tatucvieira <147344439+tatucvieira@users.noreply.github.com>
2026-02-24 00:14:08 -0300

    Auto-commit: Added main.tsx via Marvin Swarm

---
+++ stock_price_tracker_repo/src/main.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { ChakraProvider } from '@chakra-ui/react'
+import App from './App.tsx'
+import './styles/global.css'
+import theme from './styles/theme.ts'
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>,
+)
